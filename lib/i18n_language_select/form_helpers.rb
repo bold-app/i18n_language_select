87c4d6e1344a8a80d57820d45372ba33d2c0f1bc
Sarah Langheinrich <sarah@rapidrabbit.de>
2015-10-19 12:37:49 +0200

    concat to_language_code_select_tag

--- lib/i18n_language_select/form_helpers.rb
+++ lib/i18n_language_select/form_helpers.rb
@@ -2,7 +2,8 @@ module I18nLanguageSelect
   module FormHelpers
     def language_code_select(object_name, method, priority_languages = nil, options = {}, html_options = {})
       if Rails::VERSION::MAJOR >= 4
-        instance_tag = ActionView::Helpers::Tags::Select.new(object_name, method, self, [], options, html_options).to_language_code_select_tag(priority_languages, html_options)
+        # instance_tag = ActionView::Helpers::Tags::Select.new(object_name, method, self, [], options, html_options).to_language_code_select_tag(priority_languages, html_options)
+        instance_tag = ActionView::Helpers::InstanceTag.new(object_name, method, self, [], options, html_options).to_language_code_select_tag(priority_languages, html_options)
       else
         instance_tag = ActionView::Helpers::InstanceTag.new(object_name, method, self, options.delete(:object)).to_language_code_select_tag(priority_languages, html_options, options)
       end
